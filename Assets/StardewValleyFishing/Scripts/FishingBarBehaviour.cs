8d64485d730a7573ce4c0c449e9d92db4b8e28d2
Marcin Kuchta <kuchcik1920@gmail.com>
2025-04-03 02:21:32 +0200

    Add hooks behaviours to fishing bar

--- Assets/StardewValleyFishing/Scripts/FishingBarBehaviour.cs
+++ Assets/StardewValleyFishing/Scripts/FishingBarBehaviour.cs
@@ -8,29 +8,49 @@ namespace Kucheu.StardewValleyFishing
         float barPosition = 30f;
         float barSize;
         float barSpeed;
+        int numberOfBarbedHooks;
 
         public FishingBarBehaviour(FishingRodData rodData)
         {
             this.rodData = rodData;
             barSize = GetBarSize();
+            numberOfBarbedHooks = (rodData.firstTackle == TackleType.BarbedHook ? 1 : 0) + (rodData.secondTackle == TackleType.BarbedHook ? 1 : 0);
         }
 
-        public float CalculateBarPosition(bool isKeyPressed)
+        public float CalculateBarPosition(bool isKeyPressed, float fishPosition, bool isFishInBar)
         {
-            float gravity = isKeyPressed ? 0.1f : -0.1f;
+            float gravity = 0f;
             if (isKeyPressed && barPosition == 0f)
             {
                 barSpeed = 0f;
             }
+            if(isFishInBar && numberOfBarbedHooks > 0)
+            {
+                float centerOfBar = barPosition + (barSize / 2);
+                float centerOfFish = fishPosition + 16f;
+                for (int i = 0; i < numberOfBarbedHooks; i++)
+                {
+                    if (centerOfBar != centerOfFish)
+                    {
+                        float direction = (centerOfFish - centerOfBar) / Math.Abs((centerOfFish - centerOfBar));
+                        gravity += (0.02f * direction);
+                    }
+                }
+            }
+            else
+            {
+                gravity = isKeyPressed ? 0.1f : -0.1f;
+            }
             barSpeed += gravity;
             barPosition += barSpeed;
+
             if (barPosition >= 568f - barSize)
             {
                 barSpeed = 0f;
             }
             if (barPosition <= 0)
             {
-                barSpeed = -barSpeed * 2 / 3;
+                barSpeed = rodData.firstTackle == TackleType.LeadBobber || rodData.secondTackle == TackleType.LeadBobber ? 0f : (-barSpeed * 3/4);
             }
             barPosition = Math.Clamp(barPosition, 0f, 568f - barSize);
             return barPosition;
